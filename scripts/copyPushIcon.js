8973a2fb305c2fa07d93881e59db170182a68f25
nvazquezg <nvazquezg@gmail.com>
2016-10-21 16:48:35 +0200

    A custom icon for push notifications in android will be used if it exists in path 'resources/android/icon/drawable-ic_push.png'

---
+++ scripts/copyPushIcon.js
@@ -0,0 +1,37 @@
+module.exports = function(ctx) {
+    var deferral = ctx.requireCordovaModule('q').defer();
+    var filestocopy = [{
+        "../resources/android/icon/drawable-ic_push.png":
+            "../platforms/android/res/drawable/ic_push.png"
+    }];
+
+    var fs = ctx.requireCordovaModule('fs');
+    var path =  ctx.requireCordovaModule('path');
+
+// no need to configure below
+    var rootdir = process.argv[2];
+
+    filestocopy.forEach(function(obj) {
+        Object.keys(obj).forEach(function(key) {
+            var val = obj[key];
+            var srcfile = path.join(rootdir, key);
+            var destfile = path.join(rootdir, val);
+            //console.log("copying "+srcfile+" to "+destfile);
+            var destdir = path.dirname(destfile);
+            fs.mkdir(destdir);
+            
+            if (fs.existsSync(srcfile) && fs.existsSync(destdir)) {
+                fs.createReadStream(srcfile).pipe(
+                    fs.createWriteStream(destfile));
+            }
+            else if(!fs.existsSync(srcfile)){
+                console.log("Create a custom silhouette icon for push notifications in android 21+ and save it in: " + srcfile);
+            }
+            else{
+                console.log("Targer directory doesn't exist: " + destdir);
+            }
+        });
+    });
+
+    return deferral.promise;
+};
